100d18a9d99263b393736fc437e01530dc8c9697
Nicolas Garcia <nicolasgarcia960603@gmail.com>
2023-03-30 22:55:28 -0500

    move js in folder

--- UseTodos/useTodos.js
+++ UseTodos/useTodos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer } from 'react'
-import { todoReducer } from '../10-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 const initialState = [
     // {
